c6de9343ea8ebe36b91c9d1f1fcf514375178e14
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-14 01:05:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 def reverse_each_word(sentence)
-  sentence = sentence.split
-  # "collect a : #{a.collect {|x| x + "!" }}\n\n"
-  puts sentence.collect {|word| word.reverse}.join(" ")
-# word.reverse
-#   end
-  # puts sentence
-  # sentence.join(" ")
+  puts sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
 
